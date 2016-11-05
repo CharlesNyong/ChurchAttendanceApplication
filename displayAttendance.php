b14c9365e196e31bfb5d536559cca36e2021393e
Charleswils <asikpo@uwindsor.ca>
2016-11-05 06:11:38 -0400

    Update displayAttendance.php

--- displayAttendance.php
+++ displayAttendance.php
@@ -1,5 +1,6 @@
 <?php
-include("Conn.php");
+include_once("Conn.php");
+include_once("common.php");
 	class attendance{
 
 		var $dtmDate;
@@ -53,6 +54,11 @@ include("Conn.php");
 					document.getElementById("frmFilter").submit();
 				}
 				
+				function loadAll(){
+					document.getElementById("blnLoadAll").value = 1;
+					document.getElementById("frmFilter").submit();
+				}
+
 				function deleteAttendance(intAttendanceID){
 					var response = confirm("Are you sure you want to delete this attendance?");
 					if(response == true){
@@ -62,6 +68,32 @@ include("Conn.php");
 						document.getElementById("frmFilter").submit();
 						//alert("You said yes");
 					}
+				}
+
+				// function getYear(){
+
+				// }
+
+				function filterByMonth(){
+					var objMonthSelect = document.getElementById("MonthSelect");
+					var intSelectedMonth = objMonthSelect.options[objMonthSelect.selectedIndex].value;
+					var dtmYear = document.getElementById("intYear").value;
+					if(intSelectedMonth == "" || dtmYear == ""){
+						alert("Please fill in a month and year first");
+					}
+					if(intSelectedMonth != "" && dtmYear != ""){
+						document.getElementById("dtmMonth").value = dtmYear + "-" + intSelectedMonth;
+						document.getElementById("blnLoadByMonth").value = 1;
+						document.getElementById("frmFilter").submit();
+					}
+					//alert("Month Format: " + dtmYear + "-" + intSelectedMonth);
+				}
+
+				// function getSelectedValue(){
+
+				// }
+				function RefreshPage(){
+					location.reload();					
 				}
 
 				function showPopUpWindow(intAttendanceID){
@@ -88,44 +120,81 @@ include("Conn.php");
 			$strHTML = $this->javaScript();	
 			?>
 			<div class="newAttendanceBtn" onclick="showPopUpWindow(null)">New Attendance</div>
-			<hr/>
+			<span><hr/></span>
 			<form method="post" id="frmFilter" action="<?=$_SERVER['PHP_SELF'];?>">
 				<fieldset style="width:400px;">
 					<legend>Filters</legend>
-					Filter By Date: <input type="text" id="dateFilter" class="dateDropDown"></input><br/> 
+					Filter By Date: <input type="text" id="dateFilter" class="dateDropDown"></input>
 					<input type="button" value="Filter" onclick="filterByDate()"/>
+					<br/>
+					<div class="monthFilterSection">
+					Year: <input type="text" id="intYear"></input>
+					Month: <select id="MonthSelect">
+							<option value=""></option>
+							<option value="01">January</option>
+							<option value="02">Feburary</option>
+							<option value="03">March</option>
+							<option value="04">April</option>
+							<option value="05">May</option>
+							<option value="06">June</option>
+							<option value="07">July</option>
+							<option value="08">August</option>
+							<option value="09">September</option>
+							<option value="10">October</option>
+							<option value="11">November</option>
+							<option value="12">December</option>
+						</select>
+					<input type="button" value="FilterByMonth" onclick="filterByMonth()"/>
+				  </div>
+				  <input type="button" value="Show All" onclick="loadAll()"/>
+				  <input type="button" value="Refresh Site" onclick="RefreshPage()">	
 				</fieldset>
 				<input type="hidden" value="" id="dateEntered" name="dateEntered"/>
 				<input type="hidden" value="" id="attendanceID" name="intAttendanceID"/>
+				<input type="hidden" value="" id="dtmMonth" name="dtmMonth"/>
+				<input type="hidden" value="0" id="blnLoadByMonth" name="blnLoadByMonth"/>
+				<input type="hidden" value="0" id="blnLoadAll" name="blnLoadAll"/>
 				<input type="hidden" value="0" id="blnDeleteAttendance" name="blnDeleteAttendance"/>
 			</form>
-			<? var_dump($_POST); 
+			<? //var_dump($_POST);
+			session_start(); 
 			if($_POST["blnDeleteAttendance"] == 1){
-					$this->deleteAttendance($_POST["intAttendanceID"]);
-				}
-				if ($_POST["dateEntered"]) {
-					$_SESSION["dateEntered"] = $_POST["dateEntered"]; 
-				}
+				$this->deleteAttendance($_POST["intAttendanceID"]);
+			}
+			if($_POST["blnLoadAll"] == 1){
+				$this->loadAllAttendance();
+			}
 
-				if ($_POST["dateEntered"]  == "") {
-					session_start();
-					$this->loadAttendance($_SESSION["dateEntered"]);
-				}
-				else{
-					$this->loadAttendance($_POST["dateEntered"]);	
-				}
-				
+			if ($_POST["blnLoadByMonth"] == 1) {
+				$this->loadByMonth($_POST["dtmMonth"]);
+			}
+			if ($_POST["dateEntered"]) {
+				$_SESSION["dateEntered"] = $_POST["dateEntered"]; 
+			}
+
+			if ($_POST["dateEntered"]  == "") {
+				$this->loadAttendance($_SESSION["dateEntered"]);
+			}
+			else{
+				$this->loadAttendance($_POST["dateEntered"]);	
+			}
+				//var_dump($this->arrAttendanceRecords);
 			?>
-			<table border="1" style="width:900px; text-align:center;">
+			<table border="1" style="width:1350px; text-align:center;">
 				<tr>
 					<th>RowNO</th>
-					<th>Date</th>
-					<th>Day</th>
-					<th>Men</th>
-					<th>Women</th>
-					<th>Children</th>
-					<th>Total</th>
-					<th>Sunday School</th>
+					<th>DATE</th>
+					<th>SERVICE</th>
+					<th>MESSAGE</th>
+					<th>PREACHER</th>
+					<th>FIRST TIMERS</th>
+					<th>MALE</th>
+					<th>MALE STUDENTS</th>
+					<th>FEMALE</th>
+					<th>FEMALE STUDENTS</th>
+					<th>CHILDREN</th>
+					<th>TOTAL</th>
+					<th>SUNDAY SCHOOL</th>
 				</tr>
 				<?if($this->arrAttendanceRecords != NULL){
 					$intRowCount = 0;
@@ -133,17 +202,20 @@ include("Conn.php");
 					foreach ($this->arrAttendanceRecords as $intAttID => $arrRows) {?>
 					<?$intRowCount++;?>
 						<tr>
-						<td ondblclick=<?echo "deleteAttendance(".$this->intAttendanceID.")";?> ><a href="#" onclick=<?echo "showPopUpWindow(".$this->intAttendanceID.")";?> ><?echo $intRowCount;?></a></td>	
+						<td ondblclick=<?echo "deleteAttendance(".$intAttID.")";?> ><a href="#" onclick=<?echo "showPopUpWindow(".$intAttID.")";?> ><?echo $intRowCount;?></a></td>	
 						<?foreach ($arrRows as $mixKey => $mixValue) {?>
-							<?if($mixKey != "intAttendanceID"){?>
-								<td><?echo $mixValue?></td>
-							<?}?>	
+							<?if($mixKey != "intAttendanceID" && $mixKey == "dtmDate"){?>
+								<td><?echo formatDate($mixValue);?></td>
+							<?}
+							else if($mixKey != "intAttendanceID"){?>
+								<td><?echo $mixValue; ?></td>
+							<?}?>		
 						<?}?>
 						</tr> 
 					<?}
 				}
 				else{?>
-					<tr><td colspan='8'><h3 style='text-align:center;'>The is no record for that date.</h3></td></tr>
+					<tr><td colspan='13'><h3 style='text-align:center;'>The is no record for that date.</h3></td></tr>
 				<?}?>	
 			</table>
 			<? 	
@@ -163,6 +235,33 @@ include("Conn.php");
 			}	
 		}
 
+		function loadByMonth($dtmMonth){
+			global $connection;
+			$strSQL = "SELECT intAttendanceID, dtmDate, strServiceType, strMessage, strPreacher, intFirstTimer, intMale, 
+						intMaleStudents,intFemale, intFemaleStudents, intChildren, intTotal, intSundaySchool
+			 			FROM asikpo_attendance.tblChurchAttendance
+			 			WHERE dtmDate LIKE '%".$dtmMonth."%'";
+
+			$rsResult = mysqli_query($connection, $strSQL);
+
+			while ($arrRow = mysqli_fetch_assoc($rsResult)) {
+				//$this->intAttendanceID = $arrRow["intAttendanceID"];
+				$this->arrAttendanceRecords[$arrRow["intAttendanceID"]] = $arrRow;
+			}	 				
+		}	
+		function loadAllAttendance(){
+			global $connection;
+			$strSQL = "SELECT intAttendanceID, dtmDate, strServiceType, strMessage, strPreacher, intFirstTimer, intMale, 
+						intMaleStudents,intFemale, intFemaleStudents, intChildren, intTotal, intSundaySchool
+			 			FROM asikpo_attendance.tblChurchAttendance";
+			$rsResult = mysqli_query($connection, $strSQL);
+
+			while ($arrRow = mysqli_fetch_assoc($rsResult)) {
+				//$this->intAttendanceID = $arrRow["intAttendanceID"];
+				$this->arrAttendanceRecords[$arrRow["intAttendanceID"]] = $arrRow;
+			}
+		}
+
 		function loadAttendance($dtmDateToFilter){
 			global $connection;
 			if ($dtmDateToFilter) {
@@ -173,16 +272,20 @@ include("Conn.php");
 			else{
 				$this->dtmDate = null;
 			} 		
-			$strSQL = "SELECT intAttendanceID, dtmDate, strDay, intMen, intWomen, intChildren, intTotal, intSundaySchool
+			$strSQL = "SELECT intAttendanceID, dtmDate, strServiceType, strMessage, strPreacher, intFirstTimer, intMale, 
+						intMaleStudents,intFemale, intFemaleStudents, intChildren, intTotal, intSundaySchool
 			 			FROM asikpo_attendance.tblChurchAttendance
 			 			WHERE dtmDate = '$this->dtmDate' "; 
 				
 			$rsResult = mysqli_query($connection, $strSQL);
-			//$this->strSQLString = $strSQL;
+			// if($rsResult){
+			// 	//echo "Query string ". $strSQL;
+			// }
 			while ($arrRow = mysqli_fetch_assoc($rsResult)) {
 				$this->intAttendanceID = $arrRow["intAttendanceID"];
 				$this->arrAttendanceRecords[$arrRow["intAttendanceID"]] = $arrRow;
 			}
+			//echo "ID: ". $this->$intAttendanceID;
 		}
 	}
 	$objDisplayAttendance = new attendance();
