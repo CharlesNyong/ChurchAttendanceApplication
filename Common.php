030c9f219d17da0e9f29c0fa21c71369e52f5011
Charleswils <asikpo@uwindsor.ca>
2016-12-11 18:42:04 -0500

    File contains common php functions used in the application

---
+++ Common.php
@@ -0,0 +1,56 @@
+<?php
+include_once("Conn.php");
+
+if($_POST["action"]){
+	session_start();
+	$_POST["action"]();
+}
+
+$arrMonthAbbrev = array('January' => "Jan", 
+						'Feburary' => "Feb",
+						'March' => "Mar",
+						'April' => "Apr",
+						'May' => "May",
+						'June' => "June",
+						'July' => "July",
+						'August' => "Aug",
+						'September' => "Sep",
+						'October' => "Oct",
+						'November' =>"Nov",
+						'December' => "Dec");
+
+function formatDate($dtmDate){
+	global $arrMonthAbbrev;
+	$arrDateInfo = explode("-", $dtmDate);
+	$dt = DateTime::createFromFormat('!m', $arrDateInfo[1]);
+	$strMonth =  $dt->format('F');
+
+	$strFormattedDate = $arrMonthAbbrev[$strMonth] . " ". $arrDateInfo[2];
+	return $strFormattedDate;
+}
+
+function validateLimitValues($intPageNumber){
+	$intPageNumberToReturn;
+	if($intPageNumber == "" || $intPageNumber == 1){
+		$intPageNumberToReturn = 1;
+	}
+	else{
+		$intPageNumberToReturn = ($intPageNumberToReturn * 10);
+	}
+	return $intPageNumberToReturn;
+}
+
+function deleteAttendanceAjax(){
+	global $connection;
+	$intAttendanceID = $_POST["intAttendanceID"];
+	$strSQL = "DELETE FROM asikpo_attendance.tblChurchAttendance
+	 			WHERE intAttendanceID = '$intAttendanceID' "; 
+		
+	$rsResult = mysqli_query($connection, $strSQL);
+	if($rsResult){
+		$data[1] = true;
+	}	
+	echo json_encode($data);
+}
+
+?>
